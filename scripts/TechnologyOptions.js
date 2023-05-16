10a075e1c989e21c4c686c8f030819941b93ae3f
Ryan Phillips <ryanmphill@gmail.com>
2023-05-16 15:04:13 -0500

    Added event listener to TechnologyOptions.js

--- scripts/TechnologyOptions.js
+++ scripts/TechnologyOptions.js
@@ -1,13 +1,26 @@
+import { setTechnologyChoice } from "./TransientState.js"
 
+
+/*
+    Define the callback function for an event listener that targets a 'change' 
+    when the user chooses their preferred option
+*/
+const handleTechnologyChoice = (event) => {
+    // Check if the value of the item being changed is a metal
+    if (event.target.id === "technology") {
+        const chosenOption = event.target.value
+        setTechnologyChoice(parseInt(chosenOption))
+    }
+  }
 
 
 // Export a function that builds the html for the technology choices and listens for the change event
 export const TechnologyOptions = async () => {
 
     // Listen for the change event where the technology choice is being changed:
-    /*
+    
     document.addEventListener("change", handleTechnologyChoice)
-    */
+    
 
     const response = await fetch("http://localhost:8088/technologies")
     const technologies = await response.json()
