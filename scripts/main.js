0a07c754e12882c0cf4ece132c30d2ba6cde7693
Ryan Phillips <ryanmphill@gmail.com>
2023-05-16 13:44:09 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+const render = async () => {
+    
+
+    const container = document.querySelector("#container")
+
+    const composedHTML = `
+        <h1>Cars R Us</h1>
+
+        <article class="choices">
+            <section class="choices__paints options">
+                <h2>Paints</h2>
+                
+            </section>
+
+            <section class="choices__interiors options">
+                <h2>Interiors</h2>
+                
+            </section>
+
+            <section class="choices__technologies options">
+                <h2>Technologies</h2>
+                
+            </section>
+
+            <section class="choices__wheels options">
+                <h2>Wheels</h2>
+                
+            </section>
+        </article>
+
+        <article class="order">
+            
+        </article>
+
+        <article class="orderList">
+            <h2>Orders</h2>
+            
+        </article>
+    `
+
+    container.innerHTML = composedHTML
+}
